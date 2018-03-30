c0d0fab2007c038d3039cf641e38196aa210ef99
kabies <37025477+kabies@users.noreply.github.com>
2018-03-30 18:25:02 +0900

    add animation samples

---
+++ examples/animation-hitbox.rb
@@ -0,0 +1,36 @@
+require 'bismuth'
+
+class AnimationExample < Bi::Scene
+
+  def initialize
+    super
+    add_timer(1000){ Bi::Window.set_title("AnimationExample #{Bi::RunLoop.fps}FPS") }
+
+    reader = Bi::AnimationYAMLReader.new("face.yml", hitbox:[0,0xFF,0,128], hurtbox:[0xFF,0,0,128])
+    sprite = reader.default
+    hitbox_node = Bi::Node.new
+    animations = reader.read() {|frame|
+      frame.hitbox.each{|name,hitbox|
+        hitbox.boxes.each{|box| hitbox_node.add_child box }
+      }
+    }
+
+    sprite.anchor_x = sprite.anchor_y = 0.5
+    sprite.x, sprite.y = self.w/2, self.h/2
+    add_child sprite
+
+    # hitbox position start from left-top.
+    hitbox_node.x = - sprite.w * sprite.anchor_x
+    hitbox_node.y = - sprite.h * sprite.anchor_y
+    sprite.add_child hitbox_node
+
+    tmp = animations.first
+    animate = Bi::Action::animate tmp.frames, tmp.interval
+    sprite.run_action Bi::Action.repeat_forever animate
+  end
+
+end
+
+Bi::System.init(fps:60)
+Bi::Window.make_window( 320, 240)
+Bi::RunLoop.instance.run_with_scene AnimationExample.new
---
+++ examples/animation.rb
@@ -0,0 +1,27 @@
+require 'bismuth'
+
+class AnimationExample < Bi::Scene
+
+  def initialize
+    super
+    add_timer(1000){ Bi::Window.set_title("AnimationExample #{Bi::RunLoop.fps}FPS") }
+
+    reader = Bi::AnimationYAMLReader.new "face.yml"
+    sprite = reader.default
+    animations = reader.read()
+
+    sprite.anchor_x = sprite.anchor_y = 0.5
+    sprite.x = self.w/2
+    sprite.y = self.h/2
+    add_child sprite
+
+    tmp = animations.first
+    animate = Bi::Action::animate tmp.frames, tmp.interval
+    sprite.run_action Bi::Action.repeat_forever animate
+  end
+
+end
+
+Bi::System.init(fps:60)
+Bi::Window.make_window(320,240)
+Bi::RunLoop.instance.run_with_scene AnimationExample.new
